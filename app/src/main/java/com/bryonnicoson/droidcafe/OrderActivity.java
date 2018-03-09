9051f6e5582817c8d2789173a86dbd12b6705d5e
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-09 17:09:09 -0600

    Initial commit

---
+++ app/src/main/java/com/bryonnicoson/droidcafe/OrderActivity.java
@@ -0,0 +1,55 @@
+package com.bryonnicoson.droidcafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity {
+
+    private static final String TAG_ACTIVITY = OrderActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+
+    /**
+     * construct toast based on radio group selection
+     * @param view radio group of delivery methods
+     */
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.chosen) +
+                            getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.chosen) +
+                            getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.chosen) +
+                            getString(R.string.pick_up));
+                break;
+            default:
+                displayToast(getString(R.string.nothing_clicked));
+                break;
+        }
+    }
+
+    /**
+     * create a toast to display radio button clicked
+     *
+     * @param message Toast message
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
